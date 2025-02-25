c569b42daea313d6d4d9d0848146084d44f87e00
Bryant Sus Ortiz <sustaitajorge04101b@gmail.com>
2025-02-25 12:27:32 -0600

    pro nuebo

--- traslateapi/settings.py
+++ traslateapi/settings.py
@@ -95,7 +95,7 @@ WSGI_APPLICATION = 'traslateapi.wsgi.application'
 #     }
 # }
 
-
+"""base invalida"""
 DATABASES = {
     'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
